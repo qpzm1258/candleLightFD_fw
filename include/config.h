54e22df7f7dcdd91a9b4b794e36f760c14bddbab
qpzm1258 <qpzm1258@163.com>
2025-07-07 22:34:05 +0800

    boards: add RayquazaFD HUB

--- include/config.h
+++ include/config.h
@@ -369,6 +369,17 @@ THE SOFTWARE.
 	#define NUM_CAN_CHANNEL			 1
 	#define CONFIG_CANFD			 1
 
+#elif defined(BOARD_RayquazaFD)
+	#define USBD_PRODUCT_STRING_FS	 "Rayquaza FD HUB"
+	#define USBD_MANUFACTURER_STRING "RayquazaFD"
+	#define DFU_INTERFACE_STRING_FS	 "Rayquaza FD firmware upgrade interface"
+
+	#define TIM2_CLOCK_SPEED		 64000000
+
+	#define CAN_CLOCK_SPEED			 64000000
+	#define NUM_CAN_CHANNEL			 1
+	#define CONFIG_CANFD			 1
+
 #else
 	#error please define BOARD
 #endif
